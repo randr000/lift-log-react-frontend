b97cbf5ac4051eaddf77a95c520cd003c441178f
Raul Andrial <rauljandrial@gmail.com>
2023-08-10 00:04:10 -0400

    Added authentication, login only

--- src/components/SignIn.jsx
+++ src/components/SignIn.jsx
@@ -1,19 +1,33 @@
 import React, { useState } from 'react';
-import { createUserWithEmailAndPassword } from 'firebase/auth';
+import { signInWithEmailAndPassword } from 'firebase/auth';
 import { auth } from '../firebase';
 import Modal from 'react-bootstrap/Modal';
 import Button from 'react-bootstrap/Button';
 import Form from 'react-bootstrap/Form';
+import { setUserProperties } from 'firebase/analytics';
 
 const SignIn = ({showSignIn, setShowSignIn}) => {
 
     const [email, setEmail] = useState('');
     const [password, setPassword] = useState('');
+    const [error, setError] = useState(false);
 
     const hideSignIn = () => setShowSignIn(false);
 
     const handleLogin = (event) => {
         event.preventDefault();
+        signInWithEmailAndPassword(auth, email, password)
+            .then((userCredential) => {
+                // Signed in
+                const user = userCredential.user;
+                hideSignIn();
+                console.log(user);
+            })
+            .catch((error) => {
+                setError(true);
+                const errorCode = error.code;
+                const errorMessage = error.message;
+            });
     };
 
     return (
@@ -21,8 +35,8 @@ const SignIn = ({showSignIn, setShowSignIn}) => {
                 <Modal.Header closeButton>
                     <Modal.Title>Login</Modal.Title>
                 </Modal.Header>
-                <Modal.Body>
-                    <Form>
+                <Form onSubmit={handleLogin}>
+                    <Modal.Body>
                         <Form.Group className="mb-3" controlId="formBasicEmail">
                             <Form.Label>Email address</Form.Label>
                             <Form.Control type="email" placeholder="Enter email" onChange={(e) => setEmail(e.target.value)} />
@@ -31,12 +45,14 @@ const SignIn = ({showSignIn, setShowSignIn}) => {
                             <Form.Label>Password</Form.Label>
                             <Form.Control type="password" placeholder="Enter password" onChange={(e) => setPassword(e.target.value)} />
                         </Form.Group>
-                    </Form>
-                </Modal.Body>
-                <Modal.Footer>
-                    <Button variant="primary">Sign In</Button>
-                    <Button variant="secondary" onClick={hideSignIn}>Close</Button>
-                </Modal.Footer>
+                    
+                    </Modal.Body>
+                    <Modal.Footer>
+                        <Button type="submit" variant="primary">Sign In</Button>
+                        <Button variant="secondary" onClick={hideSignIn}>Close</Button>
+                        {error && <span>Wrong Email or Password</span>}
+                    </Modal.Footer>
+                </Form>
         </Modal>
     );
 };
