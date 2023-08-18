a9e4f8d95e62c54ce18288c2e7b00bc21e640a91
Raul Andrial <rauljandrial@gmail.com>
2023-08-18 00:32:35 -0400

    Created first test to check if Navbar exists

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,8 @@
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
+test('Renders Navbar component', () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+  const navBar = screen.getByRole(/navigation/i);
+  expect(navBar).toBeInTheDocument();
 });
