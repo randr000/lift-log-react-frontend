5ba0eb99ba34a1c02eede735e693491c55dfcb58
Raul Andrial <rauljandrial@gmail.com>
2023-08-18 01:26:12 -0400

    Added sample tests to make sure filename conventions were ok for Navabar and SignIn test files

---
+++ src/components/Navbar.test.jsx
@@ -0,0 +1,8 @@
+import { render, screen } from '@testing-library/react';
+import Navbar from './Navbar';
+
+test('test', () => {
+//   render(<Navbar />);
+  
+  expect(true).toEqual(true);
+});
---
+++ src/components/SignIn.test.jsx
@@ -0,0 +1,8 @@
+import { render, screen } from '@testing-library/react';
+import SignIn from './SignIn';
+
+test('test', () => {
+//   render(<SignIn />);
+  
+  expect(true).toEqual(true);
+});
