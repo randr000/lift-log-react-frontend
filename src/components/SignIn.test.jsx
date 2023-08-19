3402774ffdb92ffc68590acdaaa503a59de7c4d5
Raul Andrial <rauljandrial@gmail.com>
2023-08-19 16:56:37 -0400

    Added test to see if all sign in modal elements do not load depending on state

--- src/components/SignIn.test.jsx
+++ src/components/SignIn.test.jsx
@@ -44,3 +44,44 @@ describe('All sign in modal elements load', () => {
         expect(closeBtn).toBeInTheDocument();
     });
 });
+
+describe('All sign in modal elements do not load', () => {
+
+    const renderInitialLoad = () => {
+        render(
+            <AppContext.Provider value={{app_state: APP_INITIAL_STATE, dispatch: () => {}}}>
+                <SignIn />
+            </AppContext.Provider>
+        );
+    };
+
+    test('Login title does not load', () => {
+        renderInitialLoad();
+        const loginHeader = screen.queryByText(/login/i);
+        expect(loginHeader).not.toBeInTheDocument();
+    });
+
+    test('Email address input does not load', () => {
+        renderInitialLoad();
+        const emailInput = screen.queryByLabelText(/email address/i);
+        expect(emailInput).not.toBeInTheDocument();
+    });
+
+    test('Password input does not load', () => {
+        renderInitialLoad();
+        const passwordInput = screen.queryByLabelText(/password/i);
+        expect(passwordInput).not.toBeInTheDocument();
+    });
+
+    test('Sign In button does not load', () => {
+        renderInitialLoad();
+        const signInBtn = screen.queryByRole('button', {name: /sign in/i});
+        expect(signInBtn).not.toBeInTheDocument();
+    });
+
+    test('Close button does not load', () => {
+        renderInitialLoad();
+        const closeBtn = screen.queryByText(/close/i);
+        expect(closeBtn).not.toBeInTheDocument();
+    });
+});
