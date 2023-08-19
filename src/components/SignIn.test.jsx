a040df941614aaf1136f54360d734bb0a24f8e22
Raul Andrial <rauljandrial@gmail.com>
2023-08-19 16:53:15 -0400

    Added test to see if all sign in modal elements load depending on state

--- src/components/SignIn.test.jsx
+++ src/components/SignIn.test.jsx
@@ -1,8 +1,46 @@
 import { render, screen } from '@testing-library/react';
 import SignIn from './SignIn';
+import AppContext from '../contexts/AppContext';
+import { APP_INITIAL_STATE } from '../reducers/appReducer';
 
-test('test', () => {
-//   render(<SignIn />);
-  
-  expect(true).toEqual(true);
+describe('All sign in modal elements load', () => {
+
+    const state = {...APP_INITIAL_STATE, showSignIn: true};
+    const renderInitialLoad = () => {
+        render(
+            <AppContext.Provider value={{app_state: state, dispatch: () => {}}}>
+                <SignIn />
+            </AppContext.Provider>
+        );
+    };
+
+    test('Login title loads', () => {
+        renderInitialLoad();
+        const loginHeader = screen.getByText(/login/i);
+        expect(loginHeader).toBeInTheDocument();
+    });
+
+    test('Email address input loads', () => {
+        renderInitialLoad();
+        const emailInput = screen.getByLabelText(/email address/i);
+        expect(emailInput).toBeInTheDocument();
+    });
+
+    test('Password input loads', () => {
+        renderInitialLoad();
+        const passwordInput = screen.getByLabelText(/password/i);
+        expect(passwordInput).toBeInTheDocument();
+    });
+
+    test('Sign In button loads', () => {
+        renderInitialLoad();
+        const signInBtn = screen.getByRole('button', {name: /sign in/i});
+        expect(signInBtn).toBeInTheDocument();
+    });
+
+    test('Close button loads', () => {
+        renderInitialLoad();
+        const closeBtn = screen.getByText(/close/i);
+        expect(closeBtn).toBeInTheDocument();
+    });
 });
