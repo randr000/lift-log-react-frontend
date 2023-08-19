7545e9ceff469edf5f29e2ee08304c7fe6229288
Raul Andrial <rauljandrial@gmail.com>
2023-08-19 00:21:47 -0400

    Added tests with state in Navbar test file

--- src/components/Navbar.test.jsx
+++ src/components/Navbar.test.jsx
@@ -1,8 +1,32 @@
+import React from 'react';
 import { render, screen } from '@testing-library/react';
 import Navbar from './Navbar';
+import AppContext from '../contexts/AppContext';
+import { APP_INITIAL_STATE } from '../reducers/appReducer';
 
-test('test', () => {
-//   render(<Navbar />);
-  
-  expect(true).toEqual(true);
+describe('Sign in button renders correctly on initial load and sign out button does not', () => {
+
+    test('Sign in button renders', () => {
+    render(
+        <AppContext.Provider value={{app_state: APP_INITIAL_STATE, dispatch: () => {}}}>
+            <Navbar />
+        </AppContext.Provider>
+    );
+    const signInBtn = screen.getByRole('button', {
+        name: "Sign In"
+    });
+    expect(signInBtn).toBeInTheDocument();
+    });
+
+    test('Sign out button does not render', () => {
+        render(
+        <AppContext.Provider value={{app_state: APP_INITIAL_STATE, dispatch: () => {}}}>
+            <Navbar />
+        </AppContext.Provider>
+        );
+        const signOutBtn = screen.queryByRole('button', {
+        name: "Sign Out"
+        });
+        expect(signOutBtn).not.toBeInTheDocument();
+    });
 });
