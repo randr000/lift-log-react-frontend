ef5ceffa6dc0e16841367abfc82cc03a08562b01
Raul Andrial <rauljandrial@gmail.com>
2023-08-25 00:00:18 -0400

    Added test for sign up button on initial load

--- src/components/Navbar.test.jsx
+++ src/components/Navbar.test.jsx
@@ -3,26 +3,34 @@ import Navbar from './Navbar';
 import AppContext from '../contexts/AppContext';
 import { APP_INITIAL_STATE } from '../reducers/appReducer';
 
-describe('Sign in button renders correctly on initial load and sign out button does not', () => {
+describe('Sign in and sign up buttons render correctly on initial load and sign out button does not', () => {
 
-    test('Sign in button renders', () => {
-    render(
-        <AppContext.Provider value={{app_state: APP_INITIAL_STATE, dispatch: () => {}}}>
-            <Navbar />
-        </AppContext.Provider>
-    );
-    const signInBtn = screen.getByRole('button', {
-        name: "Sign In"
+    const initialRender = () => {
+        render(
+            <AppContext.Provider value={{app_state: APP_INITIAL_STATE, dispatch: () => {}}}>
+                <Navbar />
+            </AppContext.Provider>
+        );
+    };
+
+    test('Sign In button renders', () => {
+        initialRender();
+        const signInBtn = screen.getByRole('button', {
+            name: "Sign In"
+        });
+        expect(signInBtn).toBeInTheDocument();
     });
-    expect(signInBtn).toBeInTheDocument();
+
+    test('Sign Up button renders', () => {
+        initialRender();
+        const signUpBtn = screen.getByRole('button', {
+            name: "Sign Up"
+        });
+        expect(signUpBtn).toBeInTheDocument();
     });
 
     test('Sign out button does not render', () => {
-        render(
-        <AppContext.Provider value={{app_state: APP_INITIAL_STATE, dispatch: () => {}}}>
-            <Navbar />
-        </AppContext.Provider>
-        );
+        initialRender();
         const signOutBtn = screen.queryByRole('button', {
         name: "Sign Out"
         });
