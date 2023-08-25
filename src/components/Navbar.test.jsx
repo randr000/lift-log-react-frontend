b7ad9253705be2141e0d239376426527df4bb672
Raul Andrial <rauljandrial@gmail.com>
2023-08-25 00:08:29 -0400

    Added tests for sign in, sign up, and sign out buttons when user is signed in

--- src/components/Navbar.test.jsx
+++ src/components/Navbar.test.jsx
@@ -29,11 +29,46 @@ describe('Sign in and sign up buttons render correctly on initial load and sign 
         expect(signUpBtn).toBeInTheDocument();
     });
 
-    test('Sign out button does not render', () => {
+    test('Sign Out button does not render', () => {
         initialRender();
         const signOutBtn = screen.queryByRole('button', {
         name: "Sign Out"
         });
         expect(signOutBtn).not.toBeInTheDocument();
+    });
+});
+
+describe('Sign in and sign up buttons do not render and sign out button does when signed in', () => {
+
+    const signInRender = () => {
+        render(
+            <AppContext.Provider value={{app_state: {...APP_INITIAL_STATE, signedIn: true}, dispatch: () => {}}}>
+                <Navbar />
+            </AppContext.Provider>
+        );
+    };
+
+    test('Sign In button does not render', () => {
+        signInRender();
+        const signInBtn = screen.queryByRole('button', {
+            name: "Sign In"
+        });
+        expect(signInBtn).not.toBeInTheDocument();
+    });
+
+    test('Sign Up button does not render', () => {
+        signInRender();
+        const signUpBtn = screen.queryByRole('button', {
+            name: "Sign Up"
+        });
+        expect(signUpBtn).not.toBeInTheDocument();
+    });
+
+    test('Sign Out button does render', () => {
+        signInRender();
+        const signOutBtn = screen.getByRole('button', {
+        name: "Sign Out"
+        });
+        expect(signOutBtn).toBeInTheDocument();
     });
 });
