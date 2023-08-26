59bc0af4f2eacfdcda71798ab783e24a159501da
Raul Andrial <rauljandrial@gmail.com>
2023-08-26 01:07:17 -0400

    Added logo image and made it responsive, need to refactor

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import AppContext from './contexts/AppContext';
 import Navbar from './components/Navbar';
 import SignIn from './components/SignIn';
 import { appReducer, APP_INITIAL_STATE } from './reducers/appReducer';
+import backgroundImg from './img/liftlog-logos.jpeg';
 
 function App() {
 
@@ -10,9 +11,21 @@ function App() {
 
   return (
     <div className="App">
-      <AppContext.Provider value={{app_state: app_state, dispatch: dispatch}}>
+      <AppContext.Provider value={{app_state: app_state, dispatch: dispatch}} className="overflow-hidden">
         <Navbar />
         <SignIn />
+        {/* <div classname="container-fluid" style={{
+          backgroundImage: `url(${backgroundImg})`,
+          backgroundSize: "100% 100%",
+          backgroundRepeat: "no-repeat",
+          maxWidth: "100vw",
+          minHeight: "100vh",
+          overflowY: "hidden",
+          top: 0,
+          bottom: 0,
+        }}>
+        </div> */}
+        <img className="img-fluid" src={`${backgroundImg}`} style={{height: "100vh", width: "100vw", zIndex: -1, position: 'fixed'}}></img>
       </AppContext.Provider>
     </div>
   );
