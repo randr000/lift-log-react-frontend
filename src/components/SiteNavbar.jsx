f1530ccca43200b2847f93895aa8d8970f4e3ab9
Raul Andrial <rauljandrial@gmail.com>
2023-08-30 19:20:49 -0400

    Added routing to logo and about button

--- src/components/SiteNavbar.jsx
+++ src/components/SiteNavbar.jsx
@@ -1,9 +1,8 @@
 import React, { useContext } from "react";
 import Button from "react-bootstrap/Button";
-import Stack from "react-bootstrap/Stack";
+import { Link, useNavigate } from "react-router-dom";
 import Nav from "react-bootstrap/Nav";
 import Navbar from "react-bootstrap/Navbar";
-import NavDropdown from "react-bootstrap/NavDropdown";
 import Container from "react-bootstrap/Container";
 import AppContext from '../contexts/AppContext';
 import APP_ACTION_TYPES from "../action-types/app-action-types";
@@ -13,6 +12,8 @@ import { auth } from "../firebase";
 const SiteNavbar = () => {
 
     const {app_state, dispatch} = useContext(AppContext);
+
+    const navigate = useNavigate();
 
     const {signedIn, user} = app_state;
 
@@ -24,19 +25,29 @@ const SiteNavbar = () => {
     return (
         <Navbar expand="lg" className="bg-light">
             <Container fluid>
-                <Navbar.Brand className="google-font-800 text-center fs-3 blue-logo-color" style={{color: "#066AE1"}}>liftlog</Navbar.Brand>
+                <Navbar.Brand
+                    className="google-font-800 text-center fs-3 blue-logo-color"
+                    style={{color: "#066AE1", cursor: "pointer"}}
+                    onClick={() => navigate("/")}
+                >
+                    liftlog
+                </Navbar.Brand>
                 <Navbar.Toggle aria-controls="basic-navbar-nav" />
                 <Navbar.Collapse id="basic-navbar-nav">
                     <Nav className="me-auto">
                         {
                             !signedIn && 
                             <Nav.Item className="m-2">
-                                <Button variant="primary" onClick={() => dispatch({type: APP_ACTION_TYPES.TOGGLE_SIGN_IN_MODULE, payload: true})}>Sign In</Button>
+                                <Button variant="primary" onClick={() => dispatch({type: APP_ACTION_TYPES.TOGGLE_SIGN_IN_MODULE, payload: true})}>
+                                    Sign In
+                                </Button>
                             </Nav.Item>
                         }
                         {!signedIn && <Nav.Item className="m-2"><Button variant="success" onClick={() => {}}>Sign Up</Button></Nav.Item>}
                         {signedIn && <Nav.Item className="m-2"><Button variant="primary" onClick={handleSignOut}>Sign Out</Button></Nav.Item>}
-                        <Nav.Item className="m-2"><Button variant="primary">About</Button></Nav.Item>
+                        <Nav.Item className="m-2">
+                            <Button variant="primary" onClick={() => navigate("/about")}>About</Button>
+                        </Nav.Item>
                     </Nav>
                 </Navbar.Collapse>
             </Container>
