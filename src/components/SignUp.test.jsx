0afb193a1dd6986d212094b3ce1518d99bc9b39b
Raul Andrial <rauljandrial@gmail.com>
2023-09-11 18:09:48 -0400

    Added tests to sign up tests to see if proper elements do not load

--- src/components/SignUp.test.jsx
+++ src/components/SignUp.test.jsx
@@ -50,3 +50,50 @@ describe('All sign up modal elements load', () => {
         expect(closeBtn).toBeInTheDocument();
     });
 });
+
+describe('All sign up modal elements do not load', () => {
+
+    const renderInitialLoad = () => {
+        render(
+            <AppContext.Provider value={{app_state: APP_INITIAL_STATE, dispatch: () => {}}}>
+                <SignUp />
+            </AppContext.Provider>
+        );
+    }
+
+    test('Sign Up title does not load', () => {
+        renderInitialLoad();
+        const signUpHeader = screen.queryByTestId('sign-up-title');
+        expect(signUpHeader).not.toBeInTheDocument();
+    });
+
+    test('Email address input does not load', () => {
+        renderInitialLoad();
+        const emailInput = screen.queryByLabelText(/email address/i);
+        expect(emailInput).not.toBeInTheDocument();
+    });
+
+    test('Password input does not load', () => {
+        renderInitialLoad();
+        const passwordInput = screen.queryByLabelText(/^password$/i);
+        expect(passwordInput).not.toBeInTheDocument();
+    });
+
+    test('Confirm Password input does not load', () => {
+        renderInitialLoad();
+        const passwordInput = screen.queryByLabelText(/^confirm password$/i);
+        expect(passwordInput).not.toBeInTheDocument();
+    });
+
+    test('Sign Up button does not load', () => {
+        renderInitialLoad();
+        const signInBtn = screen.queryByRole('button', {name: /sign up/i});
+        expect(signInBtn).not.toBeInTheDocument();
+    });
+
+    test('Cancel button does not load', () => {
+        renderInitialLoad();
+        const closeBtn = screen.queryByRole('button', {name: /cancel/i});
+        expect(closeBtn).not.toBeInTheDocument();
+    });
+});
