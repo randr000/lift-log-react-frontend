9758ca5b7affe23fed56fb44d17e895647242476
Raul Andrial <rauljandrial@gmail.com>
2023-09-11 17:51:00 -0400

    Added additional necessary imports to SignUp test file

--- src/components/SignUp.test.jsx
+++ src/components/SignUp.test.jsx
@@ -1,2 +1,5 @@
 import { render, screen } from '@testing-library/react';
 import SignUp from './SignUp';
+import AppContext from '../contexts/AppContext';
+import { APP_INITIAL_STATE } from '../reducers/appReducer';
+
