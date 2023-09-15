c09e408108ae8e25fbe28f59b499990135416101
Raul Andrial <rauljandrial@gmail.com>
2023-09-14 23:07:42 -0400

    Added elements to AddExerciseCard

--- src/components/AddExerciseCard.jsx
+++ src/components/AddExerciseCard.jsx
@@ -1,9 +1,13 @@
 import React from 'react';
 import Card from 'react-bootstrap/Card';
+import Button from 'react-bootstrap/Button';
 
 const AddExerciseCard = () => {
     return (
-        <></>
+        <Card>
+            <Card.Title>Add Exercise</Card.Title>
+            <Button variant="primary">Add</Button>
+        </Card>
     );
 };
 
