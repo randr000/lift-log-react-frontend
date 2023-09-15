4931b214e387bfe950f89320bf55682d4241bb57
Raul Andrial <rauljandrial@gmail.com>
2023-09-15 09:54:14 -0400

    Added width and margin styling to AddExerciseCard

--- src/components/AddExerciseCard.jsx
+++ src/components/AddExerciseCard.jsx
@@ -4,7 +4,7 @@ import Button from 'react-bootstrap/Button';
 
 const AddExerciseCard = () => {
     return (
-        <Card>
+        <Card className="mx-5 my-3" style={{width: '18rem'}}>
             <Card.Title>Add Exercise</Card.Title>
             <Button variant="primary">Add</Button>
         </Card>
