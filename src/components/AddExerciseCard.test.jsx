014c07f6d3521e3662e93fa8058c65833f927d0e
Raul Andrial <rauljandrial@gmail.com>
2023-09-18 23:28:29 -0400

    Inserted imports to AddExerciseCard test

---
+++ src/components/AddExerciseCard.test.jsx
@@ -0,0 +1,4 @@
+import { render, screen } from '@testing-library/react';
+import AddExerciseCard from './AddExerciseCard';
+import AppContext from '../contexts/AppContext';
+import { APP_INITIAL_STATE } from '../reducers/appReducer';
