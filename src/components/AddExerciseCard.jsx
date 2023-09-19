5c0fed614906083f7dfe8ccbe666fca8bbaee27d
Raul Andrial <rauljandrial@gmail.com>
2023-09-19 16:43:47 -0400

    Changed Add to + sign in AddExerciseCard

--- src/components/AddExerciseCard.jsx
+++ src/components/AddExerciseCard.jsx
@@ -1,12 +1,18 @@
 import React from 'react';
 import Card from 'react-bootstrap/Card';
 import Button from 'react-bootstrap/Button';
+import Badge from 'react-bootstrap/Badge';
 
 const AddExerciseCard = () => {
     return (
         <Card className="mx-5 my-3" style={{width: '18rem'}}>
-            <Card.Title>Add Exercise</Card.Title>
-            <Button variant="primary">Add</Button>
+            <Card.Title className="ms-1 mt-1">Add Exercise</Card.Title>
+                <Badge
+                    pill bg="primary"
+                    className="google-font-100 text-white fs-4 w-25 align-self-center mb-1"
+                    role="button">
+                    +
+                </Badge>
         </Card>
     );
 };
