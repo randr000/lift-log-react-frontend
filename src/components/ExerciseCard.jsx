02fe38a99531f3d6242daabb9d5c52c78ebbded4
Raul Andrial <rauljandrial@gmail.com>
2023-09-23 21:56:24 -0400

    Added Card and Card Title to Exercise Card

--- src/components/ExerciseCard.jsx
+++ src/components/ExerciseCard.jsx
@@ -4,8 +4,9 @@ import Button from 'react-bootstrap/Button';
 
 const ExerciseCard = () => {
     return (
-        <>
-        </>
+        <Card className="mx-5 my-3" style={{width: '18rem'}}>
+            <Card.Title className="ms-1 mt-1">Dumbbell Flye</Card.Title>
+        </Card>
     );
 };
 
