a96e83b56ed10950bb027d9cea231443e3a0eb04
Raul Andrial <rauljandrial@gmail.com>
2023-09-23 22:10:49 -0400

    Made Expand/Collapse text change on Exercise Card Accordion when clicked

--- src/components/ExerciseCard.jsx
+++ src/components/ExerciseCard.jsx
@@ -2,14 +2,24 @@ import React from 'react';
 import Card from 'react-bootstrap/Card';
 import Button from 'react-bootstrap/Button';
 import Accordion from 'react-bootstrap/Accordion';
+import { useState } from 'react';
 
 const ExerciseCard = () => {
+
+    const [isAccordionExpanded, setIsAccordionExpanded] = useState(false);
+
+    function handleAccordionClick() {
+        setIsAccordionExpanded(state => !state);
+    }
+
     return (
         <Card className="mx-5 my-3" style={{width: '18rem'}}>
             <Card.Title className="ms-1 mt-1">Dumbbell Flye</Card.Title>
             <Accordion>
-                <Accordion.Item eventKey="0">
-                    <Accordion.Header>Expand</Accordion.Header>
+                <Accordion.Item eventKey="0" onClick={handleAccordionClick}>
+                    <Accordion.Header>
+                        {isAccordionExpanded ? "Collapse" : "Expand"}
+                    </Accordion.Header>
                     <Accordion.Body>
                         <h1>Max Weight: 15 lbs. (Each Arm)</h1>
                     </Accordion.Body>
