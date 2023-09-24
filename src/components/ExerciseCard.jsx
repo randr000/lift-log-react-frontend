f6373dee3fecd8e8ef97a67320c7c6f24b1b78de
Raul Andrial <rauljandrial@gmail.com>
2023-09-23 21:58:27 -0400

    Imported Accordion into Exercise Card

--- src/components/ExerciseCard.jsx
+++ src/components/ExerciseCard.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import Card from 'react-bootstrap/Card';
 import Button from 'react-bootstrap/Button';
+import Accordion from 'react-bootstrap/Accordion';
 
 const ExerciseCard = () => {
     return (
