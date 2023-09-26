a7f3a732d31056f266a6b13918a8f642fa531841
Raul Andrial <rauljandrial@gmail.com>
2023-09-25 21:11:06 -0400

    Updated firebase file to be able to connect to firestore db

--- src/firebase.js
+++ src/firebase.js
@@ -5,6 +5,7 @@ import { initializeApp } from "firebase/app";
 // https://firebase.google.com/docs/web/setup#available-libraries
 
 import { getAuth } from 'firebase/auth';
+import { getFirestore } from 'firebase/firestore';
 
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
@@ -20,5 +21,6 @@ const firebaseConfig = {
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
+export const db = getFirestore(app);
 // const analytics = getAnalytics(app);
 export const auth = getAuth();
