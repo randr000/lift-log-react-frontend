159182b136e4486796b9906492c0ad64570a6c94
Raul Andrial <rauljandrial@gmail.com>
2023-09-28 17:24:33 -0400

    Made buttons more responsive

--- src/components/SiteNavbar.jsx
+++ src/components/SiteNavbar.jsx
@@ -7,6 +7,7 @@ import { Router, useNavigate } from "react-router-dom";
 import Nav from "react-bootstrap/Nav";
 import Navbar from "react-bootstrap/Navbar";
 import Container from "react-bootstrap/Container";
+import Offcanvas from "react-bootstrap/Offcanvas";
 import AppContext from '../contexts/AppContext';
 import APP_ACTION_TYPES from "../action-types/app-action-types";
 import { signOut } from "firebase/auth";
@@ -26,7 +27,7 @@ const SiteNavbar = () => {
     }
 
     return (
-        <Navbar expand="lg" className="bg-light" style={{height: "7vh"}}>
+        <Navbar expand="lg" className="bg-light" style={{height: "9vh"}}>
             <Modals />
             <Container fluid>
                 <Navbar.Brand
@@ -38,28 +39,33 @@ const SiteNavbar = () => {
                     liftlog
                 </Navbar.Brand>
                 <Navbar.Toggle aria-controls="basic-navbar-nav" />
-                <Navbar.Collapse id="basic-navbar-nav" className="justify-content-end">
-                    <Nav>
-                        {
-                            !signedIn && 
-                            <Nav.Item className="m-2">
-                                <Button variant="primary" onClick={() => dispatch({type: APP_ACTION_TYPES.TOGGLE_SIGN_IN_MODAL, payload: true})}>
-                                    Sign In
-                                </Button>
+                <Navbar.Offcanvas placement="top">
+                    <Offcanvas.Header closeButton>
+                        <Offcanvas.Title />
+                    </Offcanvas.Header>
+                    <Offcanvas.Body className="justify-content-lg-end">
+                        <Nav >
+                            {
+                                !signedIn &&
+                                <Nav.Item className="m-lg-2 my-1">
+                                    <Button variant="primary" className="btn-block" onClick={() => dispatch({type: APP_ACTION_TYPES.TOGGLE_SIGN_IN_MODAL, payload: true})}>
+                                        Sign In
+                                    </Button>
+                                </Nav.Item>
+                            }
+                            {
+                                !signedIn &&
+                                <Nav.Item className="m-lg-2 my-1">
+                                    <Button variant="success" onClick={() => dispatch({type: APP_ACTION_TYPES.TOGGLE_SIGN_UP_MODAL, payload: true})}>Sign Up</Button>
+                                </Nav.Item>
+                            }
+                            {signedIn && <Nav.Item className="m-lg-2 my-1"><Button variant="primary" onClick={handleSignOut}>Sign Out</Button></Nav.Item>}
+                            <Nav.Item className="m-lg-2 my-1">
+                                <Button variant="primary" onClick={() => navigate("/about")}>About</Button>
                             </Nav.Item>
-                        }
-                        {
-                            !signedIn &&
-                            <Nav.Item className="m-2">
-                                <Button variant="success" onClick={() => dispatch({type: APP_ACTION_TYPES.TOGGLE_SIGN_UP_MODAL, payload: true})}>Sign Up</Button>
-                            </Nav.Item>
-                        }
-                        {signedIn && <Nav.Item className="m-2"><Button variant="primary" onClick={handleSignOut}>Sign Out</Button></Nav.Item>}
-                        <Nav.Item className="m-2">
-                            <Button variant="primary" onClick={() => navigate("/about")}>About</Button>
-                        </Nav.Item>
-                    </Nav>
-                </Navbar.Collapse>
+                        </Nav>
+                    </Offcanvas.Body>
+                </Navbar.Offcanvas>
             </Container>
         </Navbar>
     );
