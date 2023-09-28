2ef990bf2fa6c3b23198526c975943f5e3117daf
Raul Andrial <rauljandrial@gmail.com>
2023-09-27 22:38:43 -0400

    Able to retrieve exercise notes from firestore, pending to be able to edit, however form for it already created

--- src/components/ExerciseCard.jsx
+++ src/components/ExerciseCard.jsx
@@ -2,35 +2,46 @@ import React from 'react';
 import Card from 'react-bootstrap/Card';
 import Button from 'react-bootstrap/Button';
 import Accordion from 'react-bootstrap/Accordion';
-import { useState, useContext } from 'react';
+import { useState, useEffect, useContext } from 'react';
 import AppContext from '../contexts/AppContext';
 import APP_ACTION_TYPES from '../action-types/app-action-types';
+import { doc, onSnapshot } from 'firebase/firestore';
+import { db } from '../firebase';
 
-const ExerciseCard = ({id, name}) => {
+const ExerciseCard = ({id}) => {
 
     const {app_state, dispatch} = useContext(AppContext);
-    const {user, showViewSetsModal} = app_state;
+    const {user} = app_state;
 
     const [isAccordionExpanded, setIsAccordionExpanded] = useState(false);
+    const [exerciseDoc, setExerciseDoc] = useState({});
+
+    useEffect(() => {
+        const unsub = onSnapshot(doc(db, `users/${user.uid}/exercises/${id}`), doc => {
+            setExerciseDoc(doc.data());
+        });
+        return unsub;
+    }, []);
 
     function handleAccordionClick() {
         setIsAccordionExpanded(state => !state);
     }
 
     function handleViewSetsClick() {
-        dispatch({type: APP_ACTION_TYPES.TOGGLE_VIEW_SETS_MODAL, payload: {show: true, exerciseId: id, exerciseName: name}});
+        dispatch({type: APP_ACTION_TYPES.TOGGLE_VIEW_SETS_MODAL, payload: {
+            show: true, exerciseId: id, exerciseName: exerciseDoc.name, exerciseNotes: exerciseDoc.notes}});
     }
 
     return (
         <Card className="mx-5 my-3" style={{width: '18rem'}}>
-            <Card.Title className="ms-1 mt-1" onClick={handleViewSetsClick} style={{cursor: "pointer"}}>{name}</Card.Title>
+            <Card.Title className="ms-1 mt-1" onClick={handleViewSetsClick} style={{cursor: "pointer"}}>{exerciseDoc.name}</Card.Title>
             <Accordion>
                 <Accordion.Item eventKey="0">
                     <Accordion.Header onClick={handleAccordionClick}>
                         {isAccordionExpanded ? "Collapse" : "Expand"}
                     </Accordion.Header>
                     <Accordion.Body onClick={handleViewSetsClick} style={{cursor: "pointer"}}>
-                        <h1>Max Weight: 15 lbs. (Each Arm)</h1>
+                        <h1>{exerciseDoc.notes}</h1>
                     </Accordion.Body>
                 </Accordion.Item>
             </Accordion>
--- src/components/ViewSetsModal.jsx
+++ src/components/ViewSetsModal.jsx
@@ -1,4 +1,4 @@
-import React, { useState, useContext } from "react";
+import React, { useState, useContext, useEffect } from "react";
 import AppContext from "../contexts/AppContext";
 import Modal from 'react-bootstrap/Modal';
 import Button from 'react-bootstrap/Button';
@@ -9,15 +9,53 @@ const ViewSetsModal = () => {
 
     const {app_state, dispatch} = useContext(AppContext);
     const {showViewSetsModal} = app_state;
-    const {show, exerciseName, exerciseId} = showViewSetsModal;
+    const {show, exerciseName, exerciseId, exerciseNotes} = showViewSetsModal;
+
+    const [editNotes, setEditNotes] = useState(false);
 
     function hideModal() {
         dispatch({type: APP_ACTION_TYPES.TOGGLE_VIEW_SETS_MODAL, payload: false});
     }
 
+    function handleEditNotes() {
+        setEditNotes(true);
+    }
+
+    function handleDoneEditNotes() {
+        setEditNotes(false);
+    }
+
+    const EditNotesForm = () => {
+
+        const [notes, setNotes] = useState(exerciseNotes)
+        return (
+            <Form>
+                <Form.Control as="textarea" className="mb-2" value={notes} onChange={(e) => setNotes(e.target.value)} />
+            </Form>
+        );
+    };
+
     return (
         <Modal show={show} onHide={hideModal}>
-            <Modal.Title>{exerciseName}</Modal.Title>
+            <Modal.Header closeButton>
+                <Modal.Title>{exerciseName}</Modal.Title>
+            </Modal.Header>
+            <Modal.Body>
+                <p className="google-font-800">Exercise Notes:</p>
+                {
+                    editNotes?
+                    <>
+                        <EditNotesForm/>
+                        <Button variant="warning" onClick={handleDoneEditNotes}>Done Editing</Button>
+                    </>:
+                    <>
+                        <p className="google-font-400">{exerciseNotes}</p>
+                        <Button variant="info" onClick={handleEditNotes}>Edit Notes</Button>
+                    </>
+                }
+                <hr></hr>
+                <p className="google-font-800">Sets:</p>
+            </Modal.Body>
         </Modal>
     );
 };
